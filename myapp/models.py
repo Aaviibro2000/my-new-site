64cdee6d581ce812f3f9f397336fe86bc1cbaa9e
Aaviibro2000 <alwinshaji2020@gmail.com>
2022-10-19 15:45:49 +0530

    8th commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,6 @@
+from distutils.command.upload import upload
+from tkinter import image_types
+
 from django.db import models
 
 
@@ -7,3 +10,4 @@ class product(models.Model):
      name = models.CharField(max_length=100,unique=True)
      price = models.FloatField()
      description = models.CharField(max_length=200)
+     image= models.ImageField(blank=True,upload_to='images')
--- myapp/urls.py
+++ myapp/urls.py
@@ -4,8 +4,8 @@ from .views import add_product, index, products, products_details
 
 urlpatterns = [
     path('',index),
-    path('products',products),
-    path('products/<int:id>',products_details),
-    path('add_product/',add_product)
+    path('products',products,name='products'),
+    path('products/<int:id>',products_details,name='products_details'),
+    path('products/add',add_product,name='add_product')
     
 ]
