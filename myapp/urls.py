9d7d89ee78422d85d206313c6ca0dbd6ac1d41e2
Aaviibro2000 <alwintrack@gmail.com>
2022-12-06 18:48:57 +0530

    19th commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 
-from .views import add_product, index, products, products_details
+from .views import add_product, index, products, products_details,update_product
 
 urlpatterns = [
     path('',index),
     path('products',products,name='products'),
     path('products/<int:id>',products_details,name='products_details'),
-    path('products/add',add_product,name='add_product')
+    path('products/add',add_product,name='add_product'),
+    path('products/update/<int:id>',update_product,name='update_product'),
     
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -43,6 +43,27 @@ def add_product(request):
         return redirect('/myapp/products')
         
     return render(request,'myapp/add_product.html')
+
+def update_product(request,id):
+    p = product.objects.get(id=id)
+    context = {'p':p}
+    
+    
+    if request.method == 'POST':
+        p.name = request.POST.get('name')
+        p.price = request.POST.get('price')
+        p.description = request.POST.get('desc')
+        try:
+            p.image = request.FILES['upload']
+        except:
+            pass
+        p.save()
+        
+        return redirect('/myapp/products')
+        
+    return render(request,'myapp/update_product.html',context=context)
+
+
     
     
     
