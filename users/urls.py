0e2c68d170067d20905f4dca0dddfe1e790b0e4d
Aaviibro2000 <alwintrack@gmail.com>
2022-12-09 18:57:08 +0530

    25th commit

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from .views import register
+from django.contrib.auth import views as authentication_views
+
+app_name = 'users'
 
 
 
 urlpatterns = [
     path('register/',register,name='products'),
+    path('login/',authentication_views.LoginView.as_view(template_name='users/login.html')),
+    
     
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from users.forms import NewUserForm
-
-
 
 
 
@@ -11,10 +9,11 @@ def register(request):
     if request.method == 'POST':
         form = NewUserForm(request.POST)
         print(f'form valid : {form.is_valid()}')
-        
         if form.is_valid:
             form.save()
-        
+            
+            
+        return redirect('/myapp/products')
     form = NewUserForm(request.POST)    
     context = {
         'form' : form,
