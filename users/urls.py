fe9e48cfafbbfdc43f20677db83ae901785dc116
Aaviibro2000 <alwintrack@gmail.com>
2022-12-19 19:29:48 +0530

    28th commit

--- users/urls.py
+++ users/urls.py
@@ -9,6 +9,6 @@ app_name = 'users'
 urlpatterns = [
     path('register/',register,name='products'),
     path('login/',authentication_views.LoginView.as_view(template_name='users/login.html'),name='login'),
-    
+    path('logout/',authentication_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
     
 ]
