58212d31c00cdddd4cbee864119537a67d516e3d
Aaviibro2000 <alwintrack@gmail.com>
2022-12-21 20:50:55 +0530

    31th commit

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.db import models 
+from django.contrib.auth.admin import User
 
 
+class Profile(models.Model):
+    def __str__(self):
+        return self.user.username
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    image=models.ImageField(blank=True,upload_to='profile_pics')
+    contact_number=models.CharField(max_length=10)
